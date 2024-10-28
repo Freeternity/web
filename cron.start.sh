544ce4dceef099599a5a706d14df7e16a2c27b33
Alexander Schliker <alex.schliker@gmail.com>
2024-10-28 11:07:35 -0500

    fix

--- cron.start.sh
+++ cron.start.sh
@@ -3,6 +3,8 @@
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
 then
         export PATH=/usr/local/bin:$PATH
-        node /home/freeternity/web/index.js >> /home/freeternity/logs/node.log 2>&1
+        export secure_cookie="true"
+        export NODE_ENV="production"
+	node /home/freeternity/web/index.js >> /home/freeternity/logs/node.log 2>&1
 fi
 
