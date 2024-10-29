794dc23e34b0e0f14c541c6d6bd3fbd296eda87b
Alexander Schliker <alex.schliker@gmail.com>
2024-10-29 16:24:29 -0500

    fix

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,10 +1,17 @@
 module.exports = function(req, res, next) {
+    console.log('adminAuth middleware called');
+    console.log('Session:', req.session);
+    console.log('Body:', req.body);
+
     const { username, password } = req.body;
 
     if (username === process.env.admin_username && password === process.env.admin_password) {
+        console.log('admin Authentication successful');
         req.session.data_logged_in = true;
         return next();
     }
+
+    console.log('Authentication failed');
 
     //res.status(401).send('Unauthorized');
 };
