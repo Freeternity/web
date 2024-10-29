0a54f4c3cfc11166333af96ef7db19fbea8e553f
Alexander Schliker <alex.schliker@gmail.com>
2024-10-29 17:58:57 -0500

    cron.start.sh

--- cron.start.sh
+++ cron.start.sh
@@ -1,10 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
-then
-        export PATH=/usr/local/bin:$PATH
-        export secure_cookie="true"
-        export NODE_ENV="production"
-	node /home/freeternity/web/index.js >> /home/freeternity/logs/node.log 2>&1
-fi
+# Source the .bashrc file to load environment variables
+source ~/.bashrc
 
+# Change to the application directory
+cd /home/freeternity/web
+
+# Create logs directory if it doesn't exist
+mkdir -p ~/logs
+
+# Start the application and log output
+node index.js >> ~/logs/node.log 2>&1
+
+/home/freeternity/logs/node.log {
+    daily
+    rotate 7
+    compress
+    delaycompress
+    missingok
+    notifempty
+    create 644 freeternity freeternity
+}
