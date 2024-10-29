dcebf9cf7c9843ca9ac54f4137a53181175956c6
Alexander Schliker <alex.schliker@gmail.com>
2024-10-29 18:30:56 -0500

    some exports defined

--- cron.start.sh
+++ cron.start.sh
@@ -2,6 +2,13 @@
 
 # Source the .bashrc file to load environment variables
 source ~/.bashrc
+source ~/.profile
+
+# Set the environment variables
+export NODE_ENV=production
+export PORT=3000
+export REDIS_URL=redis://localhost:6379
+export admin_username=admin
 
 # Change to the application directory
 cd /home/freeternity/web
